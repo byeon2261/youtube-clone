2889c58272321f1fcf59deac4fba77bf7c82c8de
byeon2261 <114720002+byeon2261@users.noreply.github.com>
2023-03-09 15:55:28 +0900

    #4.2 Cleaning the Code

--- src/index.js
+++ src/index.js
@@ -1,29 +1,14 @@
 import express from "express";
 import gimochi from "morgan";
+import globalRouter from "./routes/globalRouter";
+import userRouter from "./routes/userRouter";
+import videoRouter from "./routes/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
 const logger = gimochi("dev");
 app.use(logger);
-
-const globalRouter = express.Router();
-
-const handleHome = (req, res) => res.send("Home.");
-
-globalRouter.get("/", handleHome);
-
-const userRouter = express.Router();
-
-const handleEditUser = (req, res) => res.send("Edit User");
-
-userRouter.get("/edit", handleEditUser);
-
-const videoRouter = express.Router();
-
-const handleWatchVideo = (req, res) => res.send("Watch Video");
-
-videoRouter.get("/watch", handleWatchVideo);
 
 app.use("/", globalRouter);
 app.use("/users", userRouter);
