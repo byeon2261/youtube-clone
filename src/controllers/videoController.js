83e5b5d3c3febd8c36fe7e16ac49941267986dfd
byeon2261 <114720002+byeon2261@users.noreply.github.com>
2023-03-16 14:30:36 +0900

    #4.0 Returning HTML

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,15 @@
-export const tranding = (req, res) => res.send("Tranding Video");
-export const see = (req, res) => res.send(`Watch Video #${req.params.id}`);
-export const edit = (req, res) => res.send("Edit Video");
+export const tranding = (req, res) =>
+  res.send(
+    "<!DOCTYPE html><html lang='ko'><head><title>Youtube</title></head><body><h1>Home</h1><footer>&copy; 2023 youtube -  All rights reserved</footer></body></html>"
+  );
+export const see = (req, res) =>
+  res.send(
+    `<!DOCTYPE html><html lang='ko'><head><title>Youtube</title></head><body><h1>Watch Video #${req.params.id}</h1><footer>&copy; 2023 youtube -  All rights reserved</footer></body></html>`
+  );
+export const edit = (req, res) =>
+  res.send(
+    `<!DOCTYPE html><html lang='ko'><head><title>Youtube</title></head><body><h1>Edit Video #${req.params.id}</h1><footer>&copy; 2023 youtube - All reights reserved</footer></body></html>`
+  );
 export const search = (req, res) => res.send("Search Video");
 export const upload = (req, res) => res.send("Upload Video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
