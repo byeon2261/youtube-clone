dcfa1b39b7ea6b02f11b96c99e6420c9ab086041
byeon2261 <114720002+byeon2261@users.noreply.github.com>
2023-06-07 16:31:22 +0900

    #6.18 & #6.19

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -5,10 +5,15 @@ export const home = async (req, res) => {
     res.render("home", { pageTitle: "Home", videos });
   });
 };
-export const watch = (req, res) => {
-  return res.render("watch", { pageTitle: `Watching: ` });
+export const watch = async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  const video = await Video.findById(id);
+  console.log(video);
+  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
 };
 export const getEdit = (req, res) => {
+  const { id } = req.params;
   return res.render("edit", { pageTitle: `Editing: ` });
 };
 export const postEdit = (req, res) => {
