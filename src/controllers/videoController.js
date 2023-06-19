77986ba6788892a56c8af867f86cd94a9e73a857
byeon2261 <114720002+byeon2261@users.noreply.github.com>
2023-06-19 15:38:15 +0900

    Modified Video.js to the add videoSchema.static
    #6.24 Statics

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -37,7 +37,7 @@ export const postEdit = async (req, res) => {
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags,
+    hashtags: Video.formatHashtags(hashtags),
   });
   return res.redirect(`/videos/${id}`);
 };
@@ -52,7 +52,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags,
+      hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
   } catch (error) {
